86ac2c1836d87fa6a08b9d0487d9eb75ac263323
Arman441 <007ahmadarman@gmail.com>
2022-11-08 22:13:14 +0530

    Check once there is Three codes

---
+++ sec.js
@@ -0,0 +1,17 @@
+let arr=['arman','habibi','salman']
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i])
+}
+let obj={
+    arman:21,
+    salman:19,
+    adnan:27
+    
+}
+console.log(obj)
+
+/*for mean*/
+const mean =(a,b,c,d)=>{
+    return(a+b+c+d) /4
+  }
+  console.log(mean(4,5,6,7));
